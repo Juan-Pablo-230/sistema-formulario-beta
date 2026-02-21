8f677809ac8b7450cbb3c8f0d7771dfaa5da4c60
Juan-Pablo-230 <juanib117c@gmail.com>
2026-02-20 23:50:43 -0300

    Add files via upload

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,3 @@
-console.warn("Esta es una versión BETA del sistema de inscripciones. Puede contener errores o funcionalidades incompletas. Por favor, utilícelo con precaución y reporte cualquier problema al desarrollador.");
-
 class AuthSystem {
     constructor() {
         console.log('AuthSystem MongoDB inicializado');
